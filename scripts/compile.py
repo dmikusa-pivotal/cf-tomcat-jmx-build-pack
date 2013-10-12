1b8956bc997878cb2d083d912d4223360e8e98b3
Daniel Mikusa <dmikusa@gopivotal.com>
2013-10-11 21:01:28 -0400

    Added section to copy configuration.  Added section to calculate sane memory defaults or allow overrides.  Modified start script to not background Tomcat and run with a security manager.

--- scripts/compile.py
+++ scripts/compile.py
@@ -14,8 +14,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 from build_pack_utils import Builder
+
+
+def memory_limit_to_int_mb(mem_limit_str):
+    mem = int(mem_limit_str[:-1])
+    if mem_limit_str[-1:] == 'g':
+        mem = mem * 1024
+    return mem
+
+
+def configure_catalina_opts(ctx):
+    if 'CATALINA_OPTS' in os.environ.keys():
+        ctx['CATALINA_OPTS'] = os.environ['CATALINA_OPTS']
+    if 'CATALINA_OPTS' not in ctx.keys():
+        memory_limit = memory_limit_to_int_mb(ctx['MEMORY_LIMIT'])
+        xmx = int(memory_limit * 0.6)
+        perm = int(memory_limit * 0.2)
+        ctx['CATALINA_OPTS'] = ' '.join((
+            '-Xmx%d%s' % (xmx, 'm'),
+            '-Xms%d%s' % (xmx, 'm'),
+            '-Xss256k',
+            '-XX:MaxPermSize=%d%s' % (perm, 'm'),
+            '-Dlogs.dir=%s' % ctx['LOG_DIR']))
+    if ctx['CATALINA_OPTS'].find('-Djava.io.tempdir') == -1:
+        ctx['CATALINA_OPTS'] += '-Djava.io.tempdir=$TMPDIR'
+    if ctx['CATALINA_OPTS'].find('-Dlogs.dir') == -1:
+        ctx['CATALINA_OPTS'] += '-Dlogs.dir=%s' % ctx['LOG_DIR']
+    if ctx['CATALINA_OPTS'].find('-Dhttp.port') == -1:
+        ctx['CATALINA_OPTS'] += '-Dhttp.port=$VCAP_APP_PORT'
 
 
 def log_run(cmd, retcode, stdout, stderr):
@@ -37,13 +64,21 @@ if __name__ == '__main__':
         .install()
             .package('JAVA')
             .package('TOMCAT')
+            .config()
+                .all_files()
+                .from_application('config/tomcat')
+                .or_from_build_pack('defaults/config')
+                .to('tomcat/conf')
+                .done()
             .done()
+        .execute()
+            .method(configure_catalina_opts)
         .create_start_script()
             .environment_variable()
                 .export()
                 .name('JAVA_HOME')
                 .value('JAVA_INSTALL_PATH')
             .command()
-                .run('$CATALINA_HOME/bin/startup.sh')
+                .run('$HOME/tomcat/bin/catalina.sh run -security')
                 .done()
             .write())
